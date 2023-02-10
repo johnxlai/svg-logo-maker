da724986a4b93e68c9fbdefec16b7c3dc36dad71
John Lai <lai.john.py@gmail.com>
2023-02-10 16:47:08 -0500

    Add first error check

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,6 +1,6 @@
 const Shape = require('../lib/shape');
 
-descible('Shape Tester', () => {
+describe('Shape Tester', () => {
   test('should throw an error if render() is called', () => {
     const shape = new Shape();
     const err = new Error('Child shapes must implement a render() method');
