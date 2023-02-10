8ea15d3b96006fff52f9232db8f26383398054e3
John Lai <lai.john.py@gmail.com>
2023-02-10 16:29:08 -0500

    Add prompt questions for logo style

---
+++ index.js
@@ -0,0 +1,36 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'logoName',
+      message: 'Please enter text, must not be more than 3 letters',
+    },
+    {
+      type: 'input',
+      name: 'textColour',
+      message: `Please enter text colour keyword or a hexadecimal number for the logo's test color`,
+    },
+    {
+      type: 'list',
+      name: 'logoShape',
+      message: `Please choose logo shape`,
+      choices: ['triangle', 'circle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'logoColour',
+      message: `Please enter a colour keyword or a hexadecimal number for the logo's background colour`,
+    },
+  ])
+  .then((data) => {
+    console.log(data);
+    //We want to generate the svg logo here.
+
+    // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    //   err ? console.log(err) : console.log('Success!')
+    // );
+  })
+  .catch((err) => console.error(err));
