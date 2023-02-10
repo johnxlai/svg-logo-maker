924b281fad646feb23f1796c95ead9163a97eb43
John Lai <lai.john.py@gmail.com>
2023-02-10 16:54:47 -0500

    Add Test for not empty

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,9 +1,17 @@
 const Shape = require('../lib/shape');
 
-describe('Shape Tester', () => {
+describe('Shape Error', () => {
   test('should throw an error if render() is called', () => {
     const shape = new Shape();
     const err = new Error('Child shapes must implement a render() method');
     expect(shape.render).toThrow(err);
+  });
+});
+
+describe('Shape Text', () => {
+  test('text should not be empty', () => {
+    const text = 'Jon';
+    const shape = new Shape(text);
+    expect(shape.text).toBe(text);
   });
 });
