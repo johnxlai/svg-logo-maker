24c575ea57cfbd5bd582d85fd7a9ffb8f21fe4f9
John Lai <lai.john.py@gmail.com>
2023-02-11 12:15:21 -0500

    Add shape conditions

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,7 +1,7 @@
 const Shape = require('../lib/shape');
 
 describe('Shape Error', () => {
-  test('should throw an error if render() is called', () => {
+  it('should throw an error if render() is called', () => {
     const shape = new Shape();
     const err = new Error('Child shapes must implement a render() method');
     expect(() => shape.render()).toThrow(err);
@@ -9,9 +9,17 @@ describe('Shape Error', () => {
 });
 
 describe('Shape Text', () => {
-  test('text should not be empty', () => {
-    const text = 'Jon';
+  it('should not be empty', () => {
+    const shape = new Shape('');
+    const err = new Error('Logo text cannot be empty');
+    expect(shape).toThrow(err);
+  });
+});
+
+describe('Shape Text Length', () => {
+  it('should not exceed 3 characters', () => {
+    const text = 'Max';
     const shape = new Shape(text);
-    expect(shape.text).toBe(text);
+    expect(shape.text.length);
   });
 });
