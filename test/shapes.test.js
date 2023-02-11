ed98bbcc5048daf2f78dda89075ce7dda9d8d62a
John Lai <lai.john.py@gmail.com>
2023-02-11 12:33:26 -0500

    Update test statements

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,25 +1,28 @@
 const Shape = require('../lib/shape');
 
-describe('Shape Error', () => {
-  it('should throw an error if render() is called', () => {
-    const shape = new Shape();
-    const err = new Error('Child shapes must implement a render() method');
-    expect(() => shape.render()).toThrow(err);
+describe('Shape', () => {
+  describe('Text Not Empty', () => {
+    it('should not be empty', () => {
+      const shape = () => new Shape('');
+      const err = new Error('Logo text cannot be empty');
+      expect(shape).toThrow(err);
+    });
   });
-});
 
-describe('Shape Text', () => {
-  it('should not be empty', () => {
-    const shape = new Shape('');
-    const err = new Error('Logo text cannot be empty');
-    expect(shape).toThrow(err);
+  describe('Text Length less than 3', () => {
+    it('should not exceed 3 characters', () => {
+      const text = 'Max';
+      const shape = new Shape(text);
+      expect(shape.text.length);
+    });
   });
-});
 
-describe('Shape Text Length', () => {
-  it('should not exceed 3 characters', () => {
-    const text = 'Max';
-    const shape = new Shape(text);
-    expect(shape.text.length);
+  describe('Own Render Method', () => {
+    it('should throw an error if render() is called', () => {
+      //Need to pass in text name other wise it will fail in the if statement
+      const shape = new Shape('Ted');
+      const err = new Error('Child shapes must implement a render() method');
+      expect(() => shape.render()).toThrow(err);
+    });
   });
 });
