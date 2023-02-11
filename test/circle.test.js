ba5ab0b69acc83161c51174cfe8cf6d8472ddabc
John Lai <lai.john.py@gmail.com>
2023-02-11 13:23:25 -0500

    Add Circle class

---
+++ test/circle.test.js
@@ -0,0 +1,12 @@
+const Circle = require('../lib/circle');
+
+describe('Circle', () => {
+  describe('Have own render', () => {
+    it('should have own render', () => {
+      const circle = new Circle();
+      expect(circle.render()).toBe(
+        `<polygon points="150, 18 244, 182 56, 182" fill="blue" />`
+      );
+    });
+  });
+});
