51615495e4c361bd2617adc0f4b5498fb7ec65c8
John Lai <lai.john.py@gmail.com>
2023-02-12 18:30:31 -0500

    Fix shape test js

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -17,16 +17,7 @@ describe('Shape', () => {
     });
   });
 
-  describe('Own Render Method', () => {
-    it('should throw an error if render() is called', () => {
-      //Need to pass in text name other wise it will fail in the if statement
-      const shape = new Shape('Ted', 'green', 'red');
-      const err = new Error('Child shapes must implement a render() method');
-      expect(() => shape.render()).toThrow(err);
-    });
-  });
-
-  describe('Text Colour must be vaild', () => {
+  describe('Text colour must be vaild', () => {
     it('should throw error if text colour is not a vaild css colour', () => {
       const shape = () => new Shape('Ted', 'NotColour');
       const err = new Error(
@@ -36,13 +27,22 @@ describe('Shape', () => {
     });
   });
 
-  describe('Background Colour must be vaild', () => {
+  describe('Background colour must be vaild', () => {
     it('should throw error if background colour is not a vaild colour', () => {
       const shape = () => new Shape('Ted', '#000', 'NotColour');
       const err = new Error(
         'Please enter a vaild css color keyword or hex code'
       );
       expect(shape).toThrowError(err);
+    });
+  });
+
+  describe('Own Render Method', () => {
+    it('should throw an error if render() is called', () => {
+      //Need to pass in text name other wise it will fail in the if statement
+      const shape = new Shape('Ted', 'green', 'red');
+      const err = new Error('Child shapes must implement a render() method');
+      expect(() => shape.render()).toThrow(err);
     });
   });
 });
