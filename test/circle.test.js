965c233c3745cc872003aa4206a5b9039c14aaaf
John Lai <lai.john.py@gmail.com>
2023-02-12 21:54:20 -0500

    Add make svg function

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,8 +1,8 @@
 const Circle = require('../lib/circle');
 
 describe('Circle', () => {
-  describe('Have own render', () => {
-    it('should have own render', () => {
+  describe('Render Method', () => {
+    it('should render circle', () => {
       const circle = new Circle('Jon');
       expect(circle.render()).toBe(
         `<polygon points="150, 18 244, 182 56, 182" fill="blue" />`
