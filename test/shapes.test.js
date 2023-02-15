62c85232c3b7c3721ca109939a8c005b305b1c5e
John Lai <lai.john.py@gmail.com>
2023-02-14 22:52:56 -0500

    Working with dom to refactor code, changed test cases to a for a loop, add super to child classes

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,50 +1,53 @@
 const Shape = require('../lib/shape');
-
-describe('Shape', () => {
-  describe('If input empty method', () => {
-    it('should throw an error if input is empty', () => {
-      const shape = () => new Shape();
-      const err = new Error('Input cannot be empty');
-      expect(shape).toThrow(err);
+var testCases = [
+  {
+    desc: 'should throw an error if input is empty',
+    input: {},
+    expectedErr: new Error('Input cannot be empty'),
+  },
+  {
+    desc: 'should throw error if logo text is longer than 3y',
+    input: { text: 'john' },
+    expectedErr: new Error('Logo text cannot be more than 3 characters'),
+  },
+  {
+    desc: 'should throw error if input is not a vaild css colour',
+    input: { text: 'Ted', textColour: 'NotColour' },
+    expectedErr: new Error(
+      'Please enter a vaild css color keyword or hex code'
+    ),
+  },
+  {
+    desc: 'should throw an error if render() is called',
+    input: { text: 'Ted', textColour: 'green', bgColour: 'purple' },
+    expectedErr: new Error('Child shapes must implement a render() method'),
+    shouldRender: true,
+  },
+  {
+    desc: 'Validate background colour input',
+    input: { text: 'Ted', textColour: 'green', bgColour: 'purple' },
+    expectedKey: 'bgColour',
+    expectedValue: 'purple',
+  },
+  {
+    desc: 'should add colour if it is a valid color',
+    input: { text: 'Ted', textColour: 'red', bgColour: 'purple' },
+    expectedKey: 'textColour',
+    expectedValue: 'red',
+  },
+];
+describe('Shape test suite', () => {
+  for (var tc of testCases) {
+    it(tc.desc, () => {
+      let shape = () => new Shape(tc.input);
+      if (tc.shouldRender) {
+        expect(shape.render).toThrow(tc.expectedErr);
+      } else if (tc.expectedErr) {
+        expect(shape).toThrow(tc.expectedErr);
+      } else {
+        shape = new Shape(tc.input);
+        expect(shape[tc.expectedKey]).toBe(tc.expectedValue);
+      }
     });
-  });
-
-  describe('Validate input length', () => {
-    it('should throw error if logo text is longer than 3', () => {
-      const shape = () => new Shape('John');
-      const err = new Error('Logo text cannot be more than 3 characters');
-      expect(shape).toThrowError(err);
-    });
-  });
-
-  describe('Invalid colour input', () => {
-    it('should throw error if input is not a vaild css colour', () => {
-      const shape = () => new Shape('Ted', 'NotColour');
-      const err = new Error(
-        'Please enter a vaild css color keyword or hex code'
-      );
-      expect(shape).toThrowError(err);
-    });
-  });
-
-  describe('Validate colour input', () => {
-    it('should add colour if it is a valid color', () => {
-      const shape = new Shape('Ted', 'red', '#000');
-      expect(shape.textColour).toBe('red');
-    });
-  });
-
-  describe('Validate background colour input', () => {
-    const shape = new Shape('Sam', 'black', '#123');
-    expect(shape.bgColour).toBe('#123');
-  });
-
-  describe('Own Render Method', () => {
-    it('should throw an error if render() is called', () => {
-      //Need to pass in text name other wise it will fail in the if statement
-      const shape = new Shape('You', 'green', 'rebeccapurple');
-      const err = new Error('Child shapes must implement a render() method');
-      expect(() => shape.render()).toThrow(err);
-    });
-  });
+  }
 });
