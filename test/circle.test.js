1358d6296b59a79ab77043691be57746adf42ebd
John Lai <lai.john.py@gmail.com>
2023-02-14 22:57:08 -0500

    Fix circle test js

--- test/circle.test.js
+++ test/circle.test.js
@@ -3,7 +3,12 @@ const Circle = require('../lib/circle');
 describe('Circle', () => {
   describe('Render Method', () => {
     it('should render circle string', () => {
-      const circle = new Circle('Jon', 'green', 'red');
+      const circle = new Circle({
+        text: 'Ted',
+        textColour: 'purple',
+        bgColour: 'red',
+        logoShape: 'circle',
+      });
       expect(circle.render()).toBe(
         `<circle cx="50" cy="50" r="50" fill="red" />`
       );
